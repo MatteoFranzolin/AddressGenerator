b5e5bd76d83b0f801c8671f6ade8b39192427e93
MatteoFranzolin <matteo.franzolin@iisviolamarchesini.edu.it>
2023-05-10 20:09:19 +0200

    Classe AddressGenerator completa

---
+++ ProgramAddressGenerator/ProgramAddressGenerator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramAddressGenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "00001000000000000000100000001010";
+            AddressGenerator a = new AddressGenerator(input);
+            Console.WriteLine(a.generateIPv4());
+            Console.WriteLine(a.generateSubnet());
+            Console.ReadLine();
+
+        }
+    }
+}
